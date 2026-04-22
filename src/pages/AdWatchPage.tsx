31fc9222bea619e69b41aefcf5f69c8d35691b1a
gulggum <163222604+gulggum@users.noreply.github.com>
2026-04-22 10:15:16 +0900

    미사용코드제거

--- src/pages/AdWatchPage.tsx
+++ src/pages/AdWatchPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { useReward } from "../context/RewardContext";
 import { useToast } from "../context/UIContext/ToastContext";
 
